6c10b37ddac2f65c1c4b9fb41b477428260e4b9a
anthnsm <anthony.smith353@gmail.com>
2020-09-02 21:28:36 -0700

    added button, non functional

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Form from 'react-bootstrap/Form';
 import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
 
 function Contact() {
   return (
